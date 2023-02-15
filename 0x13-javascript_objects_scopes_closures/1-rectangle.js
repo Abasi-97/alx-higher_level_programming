a0f8aaa5ff7a3f4cdeed7df3ba9ebe2693946668
Joshua Adewale <josh.jobadewale@gmail.com>
2023-02-15 19:41:54 +0100

    newfile

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a class Rectangle that defines a rectangle.
+
+module.exports = class Rectangle {
+	constructor(weight, height){
+		this.weight = weight;
+		this.height = height;
+	}
+};
+
